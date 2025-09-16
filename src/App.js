983ce0c077f55b3eed14f70f85a194b002fd3dad
RITESH HARGE <135429163+Ritesh-Harge1@users.noreply.github.com>
2025-09-16 10:29:51 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import CalendarComponent from "./components/Calendar";
+import EventFilters from "./components/EventFilters";
+import "./styles/app.css";
+
+function App() {
+  const [events, setEvents] = useState([]);
+  const [filter, setFilter] = useState("all"); // all | past | upcoming
+
+  return (
+    <div className="app">
+      <h1>Event Tracker</h1>
+      <EventFilters filter={filter} setFilter={setFilter} />
+      <CalendarComponent
+        events={events}
+        setEvents={setEvents}
+        filter={filter}
+      />
+    </div>
+  );
+}
+
+export default App;
